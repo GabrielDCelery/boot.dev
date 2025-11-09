0b05992a9d5647d0b4495b6200f9f22f8ee4f381
GaborZeller <gabriel.d.celery@gmail.com>
2025-11-09 13:37:01 +0000

    feat: moved the synchronous read and logging into channels

--- learn-http-protocol-golang/httpfromtcp/main.go
+++ learn-http-protocol-golang/httpfromtcp/main.go
@@ -8,6 +8,52 @@ import (
 	"strings"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		currentLine := ""
+
+		buffer := make([]byte, 8)
+
+		for {
+			n, err := f.Read(buffer)
+
+			if err != nil && err != io.EOF {
+				log.Fatalf("failed to read chunk into buffer, reason: %v\n", err)
+			}
+
+			if n > 0 {
+				// NOTE: We need to do the slicing because according to the docs the rest of the buffer might be used as scratch space https://pkg.go.dev/io@go1.25.4#Reader
+				chunk := string(buffer[:n])
+				parts := strings.Split(chunk, "\n")
+
+				for i, part := range parts {
+					currentLine += part
+
+					// if we are looking at the last part and there are still leftover bytes in the file then we continue reading from the file
+					if i == len(parts)-1 && err != io.EOF {
+						break
+					}
+
+					ch <- currentLine
+					currentLine = ""
+				}
+
+				clear(buffer)
+			}
+
+			if err == io.EOF {
+				break
+			}
+		}
+
+		close(ch)
+	}()
+
+	return ch
+}
+
 func main() {
 	fileName := "messages.txt"
 
@@ -19,39 +65,9 @@ func main() {
 
 	defer file.Close()
 
-	currentLine := ""
-	buffer := make([]byte, 8)
+	linesChan := getLinesChannel(file)
 
-	for {
-		n, err := file.Read(buffer)
-
-		if err != nil && err != io.EOF {
-			log.Fatalf("failed to read chunk into buffer, reason: %v\n", err)
-		}
-
-		// NOTE: even if we encounter an EOF error according to the docs we might have successfully read bytes into the buffer so first we need to handle the lefover chunk
-		if n > 0 {
-			// NOTE: We need to do the slicing because according to the docs the rest of the buffer might be used as scratch space https://pkg.go.dev/io@go1.25.4#Reader
-			chunk := string(buffer[:n])
-			parts := strings.Split(chunk, "\n")
-
-			for i, part := range parts {
-				currentLine += part
-
-				// if we are looking at the last part and there are still leftover bytes in the file then we continue reading from the file
-				if i == len(parts)-1 && err != io.EOF {
-					break
-				}
-
-				fmt.Printf("read: %s\n", currentLine)
-				currentLine = ""
-			}
-
-			clear(buffer)
-		}
-
-		if err == io.EOF {
-			break
-		}
+	for line := range linesChan {
+		fmt.Printf("read: %s\n", line)
 	}
 }
