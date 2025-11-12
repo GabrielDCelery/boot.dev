8982de70ccd43e969839bdbfc9f6497a481b7263
GaborZeller <gabriel.d.celery@gmail.com>
2025-11-12 22:02:56 +0000

    feat: added basic frame and basic test cases for RequestFromReader

--- learn-http-protocol-golang/httpfromtcp/internal/request/request_test.go
+++ learn-http-protocol-golang/httpfromtcp/internal/request/request_test.go
@@ -1,11 +1,37 @@
 package request
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRequestLineParse(t *testing.T) {
-	assert.Equal(t, "test", "test")
+	t.Run("Good GET Request line", func(t *testing.T) {
+		t.Parallel()
+		r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+		require.NoError(t, err)
+		require.NotNil(t, r)
+		assert.Equal(t, "GET", r.RequestLine.Method)
+		assert.Equal(t, "HTTP/1.1", r.RequestLine.HttpVersion)
+		assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	})
+
+	t.Run("Good GET Request line with path", func(t *testing.T) {
+		t.Parallel()
+		r, err := RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+		require.NoError(t, err)
+		require.NotNil(t, r)
+		assert.Equal(t, "GET", r.RequestLine.Method)
+		assert.Equal(t, "HTTP/1.1", r.RequestLine.HttpVersion)
+		assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	})
+
+	t.Run("Invalid number of parts in request line", func(t *testing.T) {
+		t.Parallel()
+		_, err := RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+		require.Error(t, err)
+	})
 }
