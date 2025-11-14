ad21fde160ff55a2e70afb30516c1aa69c02d3c0
GaborZeller <gabriel.d.celery@gmail.com>
2025-11-14 23:14:05 +0000

    feat: added new test cases for reading the http stream

--- learn-http-protocol-golang/httpfromtcp/internal/request/request_test.go
+++ learn-http-protocol-golang/httpfromtcp/internal/request/request_test.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"io"
 	"strings"
 	"testing"
 
@@ -56,4 +57,46 @@ func TestRequestLineParse(t *testing.T) {
 		_, err := RequestFromReader(strings.NewReader("GET / HTTP/4\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 		require.Error(t, err)
 	})
+
+	t.Run("Good GET Request line with when reading chunks of 3 bytes", func(t *testing.T) {
+		t.Parallel()
+		reader := NewChunkReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n", 3)
+		r, err := RequestFromReader(reader)
+		require.NoError(t, err)
+		require.NotNil(t, r)
+		assert.Equal(t, "GET", r.RequestLine.Method)
+		assert.Equal(t, "/", r.RequestLine.RequestTarget)
+		assert.Equal(t, "HTTP/1.1", r.RequestLine.HttpVersion)
+	})
+
+	t.Run("Good GET Request line with when reading chunks of 1 byte", func(t *testing.T) {
+		t.Parallel()
+		reader := NewChunkReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n", 1)
+		r, err := RequestFromReader(reader)
+		require.NoError(t, err)
+		require.NotNil(t, r)
+		assert.Equal(t, "GET", r.RequestLine.Method)
+		assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+		assert.Equal(t, "HTTP/1.1", r.RequestLine.HttpVersion)
+	})
+}
+
+type chunkReader struct {
+	data              string
+	numOfBytesPerRead int
+	pos               int
+}
+
+func NewChunkReader(data string, numOfBytesPerRead int) *chunkReader {
+	return &chunkReader{data: data, numOfBytesPerRead: numOfBytesPerRead, pos: 0}
+}
+
+func (cr *chunkReader) Read(p []byte) (int, error) {
+	if cr.pos >= len(cr.data) {
+		return 0, io.EOF
+	}
+	end := min(cr.pos+cr.numOfBytesPerRead, len(cr.data))
+	n := copy(p, cr.data[cr.pos:end])
+	cr.pos += n
+	return n, nil
 }
