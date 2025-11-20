ece169abc0fea4c45aac253edeb6ad74c4039308
GaborZeller <gabriel.d.celery@gmail.com>
2025-11-20 22:51:44 +0000

    feat: introduced quit and error channel for graceful shutdown

--- httpfromtcp/cmd/httplistener/main.go
+++ httpfromtcp/cmd/httplistener/main.go
@@ -16,6 +16,13 @@ func main() {
 		log.Fatalf("Error starting server: %v", err)
 	}
 	defer server.Close()
+
+	go func() {
+		if err := <-server.Err(); err != nil {
+			log.Printf("server error: %v", err)
+		}
+	}()
+
 	log.Println("Server started on port", port)
 
 	sigChan := make(chan os.Signal, 1)
--- httpfromtcp/internal/server/server.go
+++ httpfromtcp/internal/server/server.go
@@ -2,25 +2,34 @@ package server
 
 import (
 	"fmt"
-	"log"
 	"net"
 )
 
 type Server struct {
 	listener net.Listener
+	errChan  chan error
+	quitChan chan struct{}
 }
 
 func Serve(port int) (*Server, error) {
 	listener, err := net.Listen("tcp4", fmt.Sprintf(":%d", port))
+
 	if err != nil {
 		return &Server{}, fmt.Errorf("failed to create listener on port %d, reason: %v\n", port, err)
 	}
-	server := &Server{listener: listener}
+
+	server := &Server{
+		listener: listener,
+		errChan:  make(chan error, 1),
+		quitChan: make(chan struct{}),
+	}
 	go server.listen()
+
 	return server, nil
 }
 
 func (s *Server) Close() error {
+	close(s.quitChan)
 	err := s.listener.Close()
 	if err != nil {
 		return fmt.Errorf("failed to close listener: %v", err)
@@ -28,12 +37,25 @@ func (s *Server) Close() error {
 	return nil
 }
 
+func (s *Server) Err() <-chan error {
+	return s.errChan
+}
+
 func (s *Server) listen() {
 	for {
 		conn, err := s.listener.Accept()
+
 		if err != nil {
-			log.Fatalf("failed to create connection: %v", err)
+			select {
+			case <-s.quitChan:
+				// shutdown requested, exit gracefully
+				return
+			default:
+				s.errChan <- fmt.Errorf("failed to accept connection: %v", err)
+				continue
+			}
 		}
+
 		go func(conn net.Conn) {
 			defer conn.Close()
 			responseMsg := "HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" + "Content-Length: 14\r\n" + "\r\n" + "Hello World!\r\n"
